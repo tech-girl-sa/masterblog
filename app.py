5770ba5ae73a05123d5826e6dd38463f059703b5
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-02-07 17:47:11 +0100

    add delete post feature

--- app.py
+++ app.py
@@ -9,9 +9,18 @@ def get_data():
 
 def add_post(post):
     data = get_data()
+    ids = [post["id"] for post in data]
+    post["id"] = max(ids) + 1
     data.append(post)
     with open("data.json", "w", encoding="UTF-8") as handel:
         handel.write(json.dumps(data))
+
+def delete_post(post_id):
+    data = get_data()
+    filtered_data = [post for post in data if post["id"] != post_id]
+    with open("data.json", "w", encoding="UTF-8") as handel:
+        handel.write(json.dumps(filtered_data))
+
 
 @app.route('/')
 def index():
@@ -24,6 +33,11 @@ def add():
         add_post(dict(request.form))
         return redirect(url_for('index'))
     return render_template('add.html')
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    delete_post(post_id)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
