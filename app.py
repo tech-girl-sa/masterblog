b56a39d7d14b8796cd3672ae65daa1de511f875f
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-02-07 16:39:31 +0100

    add main list page and adding post page

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
+def get_data():
+    with open("data.json", "r", encoding="UTF-8") as handel:
+        return json.load(handel)
+
+def add_post(post):
+    data = get_data()
+    data.append(post)
+    with open("data.json", "w", encoding="UTF-8") as handel:
+        handel.write(json.dumps(data))
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = get_data()
+    return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        add_post(dict(request.form))
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
